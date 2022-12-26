044bd41d45c7e76c4a69d1d4c56299b928cb4b58
Mohit Bansal <mohit1291@chitkara.edu.in>
2022-12-26 11:42:16 +0530

    desgin form and submit form

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import { useState, useEffect } from "react";
+import AppBar  from "./components/AppBar.js";
+import TransactionForm from "./components/TransactionForm.js";
 
-const InitialForm={
-  amount:"",
-  description: "",
-  date: "",
-}
 function App() {
 
-  const [form, setForm] = useState(InitialForm);
+
   const [transactions, setTransactions] = useState([]);
   
   useEffect(() => {
@@ -22,49 +19,10 @@ function App() {
     setTransactions(data);
   };
 
-
-  const handleInput = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
-
-
-  // Sending data to backend
-  const handleSubmit = async (e) => {
-    e.preventDefault(); // Page not reload on submitation
-    const res = await fetch("http://localhost:4000/transaction", {
-      method: "POST",
-      body: JSON.stringify(form),
-      headers: {
-        "content-type": "application/json", // send the data in form of application/json
-      },
-    });
-    const data = await res.json();
-    console.log(data);
-    if(res.ok){
-      setForm(InitialForm)
-      fetchTransactions()     // Refresh everytime when data is added
-    }
-  };
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="number"
-          name="amount"
-          value={form.amount}
-          onChange={handleInput}
-          placeholder="Enter transactions amount"
-        />
-        <input
-          type="text"
-          name="description"
-          value={form.description}
-          onChange={handleInput}
-          placeholder="Enter transactions details"
-        />
-        <input type="date" name="date" value={form.date} onChange={handleInput} />
-        <button type="submit">Submit</button>
-      </form>
+      <AppBar/>
+      <TransactionForm fetchTransactions={fetchTransactions}/>
       <br />
       <section>
         <table>
---
+++ client/src/components/TransactionForm.js
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
+
+const InitialForm = {
+  amount: "",
+  description: "",
+  date:new Date()
+};
+
+export default function TransactionForm({fetchTransactions}) {
+  const [form, setForm] = useState(InitialForm);
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  
+  const handleDate=(newValue)=>{
+  setForm({...form,date:newValue})
+  }
+
+  // Sending data to backend
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Page not reload on submitation
+    const res = await fetch("http://localhost:4000/transaction", {
+      method: "POST",
+      body: JSON.stringify(form),
+      headers: {
+        "content-type": "application/json", // send the data in form of application/json
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+    if (res.ok) {
+      setForm(InitialForm);
+      fetchTransactions(); // Refresh everytime when data is added
+    }
+  };
+  return (
+    <Card sx={{ minWidth: 275, marginTop: 10 }}>
+      <CardContent>
+        <Typography variant="h6">Add New Transaction</Typography>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            sx={{ marginRight: 5 }}
+            size="small"
+            id="outlined-basic"
+            label="Amount"
+            variant="outlined"
+            name="amount"
+            value={form.amount}
+            onChange={handleChange}
+          />
+          <TextField
+            sx={{ marginRight: 5 }}
+            size="small"
+            id="outlined-basic"
+            label="Description"
+            variant="outlined"
+            name="description"
+            value={form.description}
+            onChange={handleChange}
+          />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DesktopDatePicker
+              label="Transaction Date "
+              inputFormat="MM/DD/YYYY"
+              value={form.date}
+              onChange={handleDate}
+              renderInput={(params) => (
+                <TextField size="small"  sx={{ marginRight: 5 }} {...params} />
+              )}
+            />
+          </LocalizationProvider>
+          <Button type="submit" variant="contained">
+            Submit
+          </Button>
+        </form>
+      </CardContent>
+    </Card>
+  );
+}
