52f62bf3c0a62f2866d41c4eae9106795da7b31b
Mohit Bansal <mohit1291@chitkara.edu.in>
2023-01-04 23:45:59 +0530

    done

--- server.js
+++ server.js
@@ -16,18 +16,24 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 passportConfig(passport);
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
 app.use("/", routes);
 
 await connect();
-const __dirname1=path.resolve();
-app.use(express.static(path.join(__dirname1, "./client/build")));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname1, "./client/build/index.html"));
-});
+// deployment
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "./client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "./client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
 });
