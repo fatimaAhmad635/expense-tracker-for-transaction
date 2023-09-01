49eb890aadb436b9f57304e10c70bb5b5993cc38
Mohit Bansal <mohit1291@chitkara.edu.in>
2023-09-01 13:29:18 +0530

    Add comments for code clarity and developer understanding

--- database/mongdb.js
+++ database/mongdb.js
@@ -1,14 +1,22 @@
+// Import Mongoose library
 import mongoose from "mongoose";
 
+// Define an asynchronous function to establish a MongoDB connection
 async function connect() {
-  const username = process.env.MONGO_DB_USERNAME;
-  const passport = process.env.MONGO_DB_PASSWORD;
-  const url = process.env.MONGO_DB_URL;
+  // Retrieve MongoDB connection details from environment variables
+  const username = process.env.MONGO_DB_USERNAME; // MongoDB username
+  const password = process.env.MONGO_DB_PASSWORD; // MongoDB password
+  const url = process.env.MONGO_DB_URL; // MongoDB URL
 
-  await mongoose.connect(
-    `mongodb+srv://${username}:${passport}@${url}/?retryWrites=true&w=majority`
-  );
+  // Construct the MongoDB connection URI with username and password
+  const mongoURI = `mongodb+srv://${username}:${password}@${url}/?retryWrites=true&w=majority`;
+
+  // Attempt to connect to the MongoDB database using Mongoose
+  await mongoose.connect(mongoURI);
+
+  // Log a success message if the connection is established
   console.log("MongoDB connection is successful");
 }
 
+// Export the 'connect' function for use in your application
 export default connect;
