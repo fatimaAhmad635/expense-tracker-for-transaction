11b5a37fbe9a7390c2b488c5fc3711578b00032d
Mohit Bansal <mohit1291@chitkara.edu.in>
2023-09-01 13:20:25 +0530

    Add comments for code clarity and developer understanding

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Import necessary libraries and modules
 import bodyParser from "body-parser";
 import cors from "cors";
 import * as dotenv from "dotenv";
@@ -7,32 +8,49 @@ import passportConfig from "./config/passport.js";
 import connect from "./database/mongdb.js";
 import routes from "./routes/index.js";
 import path from "path";
+
+// Load environment variables from a .env file
 dotenv.config();
 
+// Set the server's port, defaulting to 4000 if not specified in the environment
 const PORT = process.env.PORT || 4000;
+
+// Create an Express application
 const app = express();
+
+// Middleware: Enable CORS for cross-origin requests
 app.use(cors());
+
+// Middleware: Parse incoming JSON requests
 app.use(bodyParser.json());
+
+// Middleware: Initialize Passport for authentication
 app.use(passport.initialize());
 passportConfig(passport);
 
+// Use the defined routes for routing
 app.use("/", routes);
 
+// Connect to the MongoDB database
 await connect();
 
-// deployment
+// Deployment configuration
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
+  // Serve static files from the 'client/build' directory
   app.use(express.static(path.join(__dirname1, "./client/build")));
 
+  // Handle all other routes by serving the 'index.html' file
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname1, "./client", "build", "index.html"))
   );
 } else {
+  // In development mode, respond with a simple message for the root route
   app.get("/", (req, res) => {
     res.send("API is running..");
   });
 }
+// Start the server and listen on the specified ports
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
 });
