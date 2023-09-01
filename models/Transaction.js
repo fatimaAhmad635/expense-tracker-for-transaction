d744b50f2393f34d3fede44b3efc7079f4222bfb
Mohit Bansal <mohit1291@chitkara.edu.in>
2023-09-01 13:27:29 +0530

    Add comments for code clarity and developer understanding

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,18 +1,24 @@
+// Import Mongoose library
 import mongoose from "mongoose";
+
+// Extract the 'Schema' class from Mongoose
 const { Schema } = mongoose;
 
+// Define the transaction schema using the Schema class
 const transactionSchema = new Schema({
-  amount: Number,
-  description: String,
+  // Define the schema fields for the 'Transaction' model
+  amount: Number, // Transaction amount
+  description: String, // Description of the transaction
   type: {
     type: String,
-    enum: ["expense", "income", "transfer"],
-    default: "expense",
+    enum: ["expense", "income", "transfer"], // Transaction type can be 'expense', 'income', or 'transfer'
+    default: "expense", // Default type is 'expense'
   },
-  user_id: mongoose.Types.ObjectId,
-  category_id: mongoose.Types.ObjectId,
-  date: { type: Date, default: new Date() },
-  createdAt: { type: Date, default: Date.now },
+  user_id: mongoose.Types.ObjectId, // Reference to the user who made the transaction
+  category_id: mongoose.Types.ObjectId, // Reference to the category of the transaction
+  date: { type: Date, default: new Date() }, // Transaction date, default is the current date
+  createdAt: { type: Date, default: Date.now }, // Timestamp for when the transaction was created
 });
 
+// Create and export the 'Transaction' model using the defined schema
 export default new mongoose.model("Transaction", transactionSchema);
--- models/User.js
+++ models/User.js
@@ -1,15 +1,21 @@
+// Import Mongoose library
 import mongoose from "mongoose";
+
+// Extract the 'Schema' class from Mongoose
 const { Schema } = mongoose;
 
+// Define the user schema using the Schema class
 const userSchema = new Schema(
   {
+    // Define the schema fields for the 'User' model
     firstName: { type: String, required: ["First name field is required"] },
     lastName: { type: String, required: ["Last name field is required"] },
     email: { type: String, required: ["Email field is required"] },
     password: { type: String, required: ["Password field is required"] },
-    categories: [{ label: String, icon: String }],
+    categories: [{ label: String, icon: String }], // An array of categories with 'label' and 'icon' fields
   },
-  { timestamps: true }
+  { timestamps: true } // Enable timestamps for 'createdAt' and 'updatedAt'
 );
 
+// Create and export the 'User' model using the defined schema
 export default new mongoose.model("User", userSchema);
