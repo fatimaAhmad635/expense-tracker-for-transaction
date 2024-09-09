87548ad6d4415a9a8873906ba181a982cb2c45c0
fatimaAhmad635 <fa7227508@gmail.com>
2024-09-09 15:23:55 +0500

    Update TransactionApi.js

--- routes/TransactionApi.js
+++ routes/TransactionApi.js
@@ -2,17 +2,10 @@ import { Router } from "express";
 import * as TransactionController from "../controller/TransactionController.js";
 const router = Router();
 
-// sending json response to /transaction  when http GET request to given URL
 router.get("/", TransactionController.index);
-
-router.get("/:id",TransactionController.filter);
-
-// create transaction using /transaction url
+router.get("/:id", TransactionController.filter);
 router.post("/", TransactionController.create);
-
-// Delete transaction using /transaction/:id
 router.delete("/:id", TransactionController.destroy);
-
-// Update transaction using /transaction/:id
 router.patch("/:id", TransactionController.update);
+
 export default router;
