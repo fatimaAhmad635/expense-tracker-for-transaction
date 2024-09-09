f8163d587b4a35087c2a6d41365c656d0d5fc775
fatimaAhmad635 <fa7227508@gmail.com>
2024-09-09 19:13:30 +0500

    Update NotificationSystem.js

--- client/src/components/NotificationSystem.js
+++ client/src/components/NotificationSystem.js
@@ -1,5 +1,5 @@
 // src/components/NotificationSystem.js
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
