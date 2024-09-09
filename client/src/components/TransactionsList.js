7ac08a1cf52bda266d7591776a649c232f7897a0
fatimaAhmad635 <fa7227508@gmail.com>
2024-09-09 15:15:47 +0500

    Update TransactionsList.js

--- client/src/components/TransactionsList.js
+++ client/src/components/TransactionsList.js
@@ -17,12 +17,12 @@ import * as React from "react";
 import { useSelector } from "react-redux";
 
 // Define the TransactionsList component
-export default function TransactionsList({ data, fetchTransctions, setEditTransaction,setCategoryFilter }) {
+export default function TransactionsList({ data, fetchTransactions, setEditTransaction, setCategoryFilter }) {
   // Get the user data from the Redux store
   const user = useSelector((state) => state.auth.user);
   const [selectedCategory, setSelectedCategory] = React.useState(null);
 
-  const handleChange = (event,value) => {
+  const handleChange = (event, value) => {
     if (value) {
       console.log("Selected value: " + value._id);
       setSelectedCategory(value); // Update local state with selected category
@@ -43,11 +43,9 @@ export default function TransactionsList({ data, fetchTransctions, setEditTransa
   // Function to remove a transaction
   async function remove(_id) {
     const token = Cookies.get("token");
-    
-    // Display a confirmation dialog before deletion
+
     if (!window.confirm("Are you sure")) return;
-    
-    // Send a DELETE request to remove the transaction
+
     const res = await fetch(`${process.env.REACT_APP_BASE_URL}/transaction/${_id}`, {
       method: "DELETE",
       headers: {
@@ -56,8 +54,7 @@ export default function TransactionsList({ data, fetchTransctions, setEditTransa
     });
 
     if (res.ok) {
-      // Fetch transactions again to update the list
-      fetchTransctions();
+      fetchTransactions(); // Refresh transactions
       window.alert("Deleted Successfully");
     }
   }
@@ -67,61 +64,49 @@ export default function TransactionsList({ data, fetchTransctions, setEditTransa
     return dayjs(date).format("DD MMM, YYYY");
   }
 
-  
-
   return (
     <>
-      {/* Display a title for the list of transactions */}
       <Typography sx={{ marginTop: 10 }} variant="h6">
         List of Transactions
       </Typography>
-      {/* <Box sx={{ float:'right',marginTop:-4,marginBottom:2 }}> */}
+
       <Autocomplete
-            isOptionEqualToValue={(option, value) => option._id === value._id}
-            value={selectedCategory}
-            onChange={handleChange}
-            id="controllable-states-demo"
-            options={user.categories}
-            sx={{ width: 200, float:'right',marginTop:-4,marginBottom:2}}
-            renderInput={(params) => <TextField {...params} size="small" label="Category" />}
-        />
-      {/* Create a table to display the transactions */}
+        isOptionEqualToValue={(option, value) => option._id === value._id}
+        value={selectedCategory}
+        onChange={handleChange}
+        id="category-filter"
+        options={user.categories}
+        sx={{ width: 200, float: 'right', marginTop: -4, marginBottom: 2 }}
+        renderInput={(params) => <TextField {...params} size="small" label="Category" />}
+      />
+
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          {/* Define the table header */}
           <TableHead>
             <TableRow>
               <TableCell align="center">Amount</TableCell>
+              <TableCell align="center">Currency</TableCell>
               <TableCell align="center">Description</TableCell>
               <TableCell align="center">Category</TableCell>
               <TableCell align="center">Date</TableCell>
               <TableCell align="center">Action</TableCell>
             </TableRow>
           </TableHead>
-
-          {/* Populate the table with transaction data */}
           <TableBody>
             {data.map((month) =>
               month.transactions.map((row) => (
                 <TableRow key={row._id} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-                  <TableCell align="center" component="th" scope="row">
-                    {`$${row.amount}`}
-                  </TableCell>
+                  <TableCell align="center">{`$${row.amount}`}</TableCell>
+                  <TableCell align="center">{row.currency}</TableCell>
                   <TableCell align="center">{row.description}</TableCell>
                   <TableCell align="center">{categoryName(row.category_id)}</TableCell>
                   <TableCell align="center">{formatDate(row.date)}</TableCell>
                   <TableCell align="center">
-                    {/* Edit and Delete buttons for each transaction */}
-                    <IconButton
-                      color="primary"
-                      component="label"
-                      onClick={() => setEditTransaction(row)}
-                    >
-                      <EditSharpIcon />
-                    </IconButton>
-
-                    <IconButton color="warning" component="label" onClick={() => remove(row._id)}>
+                    <IconButton onClick={() => remove(row._id)} aria-label="delete" size="large">
                       <DeleteSharpIcon />
+                    </IconButton>
+                    <IconButton onClick={() => setEditTransaction(row)} aria-label="edit" size="large">
+                      <EditSharpIcon />
                     </IconButton>
                   </TableCell>
                 </TableRow>
